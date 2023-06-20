2ca8d5dfbf03b03841d898785774e2b0a7a76e9c
RitGen <rmshkula@gmail.com>
2023-06-20 17:23:35 +0300

    Не отображается стартовый фрагмент

--- app/src/main/java/com/example/pet_coctails/fragments/OnboardFirstFragment.kt
+++ app/src/main/java/com/example/pet_coctails/fragments/OnboardFirstFragment.kt
@@ -5,17 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.pet_coctails.R
+import com.example.pet_coctails.databinding.FragmentOnboardFirstBinding
 
 class OnboardFirstFragment : Fragment() {
+
     private lateinit var binding: FragmentOnboardFirstBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentOnboardFirstBinding.inflate(inflater, container, false)
-        return binding.root
+//        binding = FragmentOnboardFirstBinding.inflate(inflater, container, false)
+//        return binding.root
+        return inflater.inflate(R.layout.fragment_onboard_first, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnNext.setOnClickListener {
+            findNavController().navigate(R.id.action_onboardFirstFragment_to_onboardSecondFragment)
+        }
 
     }
 }
