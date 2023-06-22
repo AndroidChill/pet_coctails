90f6c0f06d1c74c46011a1aa7d5c8e1cd80d22d0
Nikita <androidChill@yandex.ru>
2023-06-22 20:34:08 +0400

    test

--- app/src/main/java/com/example/pet_coctails/activity/main/MainActivity.kt
+++ app/src/main/java/com/example/pet_coctails/activity/main/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.pet_coctails.activity.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pet_coctails.R
+import com.example.pet_coctails.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
