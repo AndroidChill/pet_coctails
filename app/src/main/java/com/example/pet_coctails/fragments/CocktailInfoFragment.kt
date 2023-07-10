ec42aa9b4c2f0a7d3c8901d10b367e976357e172
RitGen <rmshkula@gmail.com>
2023-07-10 17:58:01 +0300

    Выстрадал слушатель нажатий для итема, но опять всё работает не так как хотелось бы

--- app/src/main/java/com/example/pet_coctails/fragments/CocktailInfoFragment.kt
+++ app/src/main/java/com/example/pet_coctails/fragments/CocktailInfoFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.pet_coctails.R
 import com.example.pet_coctails.databinding.FragmentCocktailInfoBinding
 import com.example.pet_coctails.databinding.FragmentCocktailsListBinding
@@ -21,6 +22,9 @@ class CocktailInfoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.btnBack.setOnClickListener {
+            findNavController().navigate(R.id.action_cocktailInfoFragment_to_cocktailsListFragment)
+        }
 
     }
 }
