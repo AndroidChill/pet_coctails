0ad819ba2be34f4bf1a2da2a4c38ff9a65388788
RitGen <rmshkula@gmail.com>
2023-07-15 20:23:23 +0300

    Сделал BaseFragment и Переделал CocktailsListFragment

--- app/src/main/java/com/example/pet_coctails/core/abstraction/BaseFragment.kt
+++ app/src/main/java/com/example/pet_coctails/core/abstraction/BaseFragment.kt
@@ -1,5 +1,50 @@
 package com.example.pet_coctails.core.abstraction
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewbinding.ViewBinding
+import javax.inject.Inject
 
-//TODO сделать базовый фрагмент по аналогии с базовой активити (проговаривалось в конце записи от 14.07)
-class BaseFragment {}
+
+//TODO  базовый фрагмент по аналогии с базовой активити (проговаривалось в конце записи от 14.07 на 52.50)
+abstract class BaseFragment <V: ViewBinding, VM: ViewModel> : AppCompatActivity () {
+
+    private var _binding: V? = null
+    protected val binding: V
+        get() = requireNotNull(_binding)
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private lateinit var _viewModel: VM
+    protected val viewModel: VM
+        get() = _viewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) { //TODO не хочет проглатывать onCreateView
+        setupDaggerComponent()
+        super.onCreate(savedInstanceState)
+
+        if (_binding == null) {
+            _binding = getViewBinding(layoutInflater)
+        }
+        setContentView(_binding?.root)
+        _viewModel = ViewModelProvider(this, viewModelFactory)[getViewModelClass]
+
+        initUI()
+    }
+
+    open fun initUI() {}
+
+    abstract fun setupDaggerComponent()
+
+    override fun onDestroy() { //TODO не хочет проглатывать onDestroyView
+        super.onDestroy()
+        _binding = null
+    }
+
+    abstract val getViewBinding: (LayoutInflater) -> V
+    abstract val getViewModelClass: Class<VM>
+}
--- app/src/main/java/com/example/pet_coctails/fragments/coctailsList/CocktailsListFragment.kt
+++ app/src/main/java/com/example/pet_coctails/fragments/coctailsList/CocktailsListFragment.kt
@@ -1,84 +1,61 @@
 package com.example.pet_coctails.fragments.coctailsList
 
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewbinding.ViewBinding
 import com.example.pet_coctails.R
+import com.example.pet_coctails.activity.main.MainViewModel
+import com.example.pet_coctails.core.abstraction.BaseFragment
+import com.example.pet_coctails.databinding.ActivityMainBinding
 import com.example.pet_coctails.databinding.FragmentCocktailsListBinding
+import com.example.pet_coctails.features.auth.BaseApplication
+import com.example.pet_coctails.features.auth.di.DaggerAuthComponent
+import com.example.pet_coctails.fragments.MainFragment
+import com.example.pet_coctails.fragments.coctailsList.api.CocktailsViewModel
 
 // TODO Как viewModel вызывать? Наследоваться не хочет
 
-class CocktailsListFragment : Fragment(), CocktailsAdapter.Listener {
+class CocktailsListFragment : BaseFragment<FragmentCocktailsListBinding, CocktailsViewModel>(), CocktailsAdapter.Listener {
 
-    private lateinit var binding: FragmentCocktailsListBinding
+//    private lateinit var binding: FragmentCocktailsListBinding
+
+    override val getViewBinding: (LayoutInflater) -> FragmentCocktailsListBinding
+        get() = FragmentCocktailsListBinding::inflate
+
+    override val getViewModelClass: Class <CocktailsViewModel>
+        get() = CocktailsViewModel::class.java
+
+
+    override fun setupDaggerComponent() {
+        val authComponent = DaggerAuthComponent
+            .builder()
+            .coreComponent((application as BaseApplication).getCoreComponent())
+            .build()
+
+        authComponent.inject(this)
+    }
 
     private val cocktailsAdapter = CocktailsAdapter(this)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentCocktailsListBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+    override fun initUI() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, MainFragment())
+            .commit()
 
-    private val data = listOf(
-        CocktailsData(
-            imageLink = R.mipmap.ic_launcher_round,
-            cocktailName = "Samba",
-            id = 345867,
-            category = "short drink",
-            cocktailType = "light",
-            glassType = "small",
-            info = "Open cocktail info"
-        ),
-        CocktailsData(
-            imageLink = R.mipmap.ic_launcher_round,
-            cocktailName = "Samba",
-            id = 345867,
-            category = "short drink",
-            cocktailType = "light",
-            glassType = "small",
-            info = "Open cocktail info"
-        ),
-        CocktailsData(
-            imageLink = R.mipmap.ic_launcher_round,
-            cocktailName = "Samba",
-            id = 345867,
-            category = "short drink",
-            cocktailType = "light",
-            glassType = "small",
-            info = "Open cocktail info"
-        ),
-        CocktailsData(
-            imageLink = R.mipmap.ic_launcher_round,
-            cocktailName = "Samba",
-            id = 345867,
-            category = "short drink",
-            cocktailType = "light",
-            glassType = "small",
-            info = "Open cocktail info"
-        ),
-        CocktailsData(
-            imageLink = R.mipmap.ic_launcher_round,
-            cocktailName = "Samba",
-            id = 345867,
-            category = "short drink",
-            cocktailType = "light",
-            glassType = "small",
-            info = "Open cocktail info"
-        ),
-    )
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+//        cocktailsAdapter.addData(
+//
+//        )
 
-        cocktailsAdapter.addData(
-            data
-        )
-
-        binding.rvCocktails.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvCocktails.layoutManager = LinearLayoutManager(requireContext()) // todo а что не так?(
 
         binding.rvCocktails.adapter = cocktailsAdapter
 
