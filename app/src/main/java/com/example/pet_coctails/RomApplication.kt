3bbdcd2be9031200da19dd14956fb9eb266eaa7d
Nikita <androidChill@yandex.ru>
2023-07-19 20:49:24 +0400

    oh tish

--- app/src/main/java/com/example/pet_coctails/RomApplication.kt
+++ app/src/main/java/com/example/pet_coctails/RomApplication.kt
@@ -1,26 +1,30 @@
 package com.example.pet_coctails
 
-
 import com.example.pet_coctails.core.scope.CoreComponent
 import com.example.pet_coctails.di.component.ApplicationComponent
 import com.example.pet_coctails.di.component.DaggerApplicationComponent
 import com.example.pet_coctails.di.module.ApplicationModule
 import com.example.pet_coctails.features.auth.BaseApplication
 
+/**
+ * загрузка изображений с проогрессбаром
+ * onTouchEvent in CocktailInfoFragment - распознать скролл вниз и при удачном скролле показывать BottomSheetFragmentDialog
+ * сердечко в списке коктелей
+ * отдельный экран для рандомного коктеля и свайпом получать новый рандомный коктейль
+ * coroutineexcHandler
+ * посмотреть на room
+ * */
+
 class RomApplication : BaseApplication() {
 
 //    private lateinit var applicationComponent: ApplicationComponent
-
+    
     val appComponent: ApplicationComponent by lazy {
-        DaggerApplicationComponent
-            .builder()
-            .applicationModule(ApplicationModule(this))
-            .build()
+        DaggerApplicationComponent.builder().applicationModule(ApplicationModule(this)).build()
     }
-
+    
     var timeClosed = 0L
-
-
+    
     override fun onCreate() {
         super.onCreate()
         injectDependencies()
@@ -53,17 +57,17 @@ class RomApplication : BaseApplication() {
 //            override fun onActivityDestroyed(p0: Activity) {}
 //        })
     }
-
+    
     private fun isWorkSleptActivity(): Boolean {
         return timeClosed == 0L || (System.currentTimeMillis() - timeClosed > 30_000)
     }
-
+    
     override fun getCoreComponent(): CoreComponent {
         return appComponent.baseComponent().create()
     }
-
+    
     private fun injectDependencies() {
         appComponent.inject(this)
     }
-
+    
 }
--- app/src/main/java/com/example/pet_coctails/fragments/cocktailsList/CocktailsListViewHolder.kt
+++ app/src/main/java/com/example/pet_coctails/fragments/cocktailsList/CocktailsListViewHolder.kt
@@ -1,14 +1,37 @@
 package com.example.pet_coctails.fragments.cocktailsList
 
 import androidx.recyclerview.widget.RecyclerView
+import coil.ImageLoader
 import coil.load
+import coil.request.ImageRequest
+import com.example.pet_coctails.R
 import com.example.pet_coctails.databinding.ItemCocktailBinding
 
 class CocktailsListViewHolder (private val binding: ItemCocktailBinding): RecyclerView.ViewHolder(binding.root) {
 
     fun itemCocktail (imageLink: String, cocktailName: String, id: String, category: String, cocktailType: String, glassType: String){
         with(binding){
-            ivCocktail.load (imageLink)
+            val request = ImageRequest.Builder(ivCocktail.context)
+                .data(imageLink)
+                .target(
+                    onStart = { placeholder ->
+                        ivCocktail.setImageDrawable(ivCocktail.context.getDrawable(R.drawable.ic_launcher_foreground))
+                    },
+                    onSuccess = { result ->
+                        ivCocktail.setImageDrawable(result)
+                    },
+                    onError = { error ->
+                        // Handle the error drawable.
+                    }
+                )
+                .build()
+            
+            val imageLoader = ImageLoader.Builder(ivCocktail.context)
+                .crossfade(true)
+                .build()
+            
+            imageLoader.enqueue(request)
+            
             tvName.text = "Cocktail name: $cocktailName"
             tvId.text = "Cocktail id: $id"
             tvCategory.text = "Cocktail category: $category"
